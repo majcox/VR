87329d3fc538d735a5e9f2c372e9609f2d3d23f9
majcox <petkq.majercik@gmail.com>
2020-05-13 18:16:44 +0200

    gdfgdgd

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,18 @@ var rotationSpeed = 0.3;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x;y;z += rotationSpeed;
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
+}
+
+function spin(){
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.y);
+}
+
+function spin(){
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.z);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
